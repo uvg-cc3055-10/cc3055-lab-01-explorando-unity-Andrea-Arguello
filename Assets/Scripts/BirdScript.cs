22e2c2bd5731d5ce9b3117a709c141743fe151e3
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-04-10 08:31:49 -0600

    Juego de Flappy Bird terminado

---
+++ Assets/Scripts/BirdScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour {
+
+    public float forwardSpeed = 2f;
+    public float jumpForce = 100f;
+    public Rigidbody2D rb;
+    public GameObject cam;
+    public Boolean dead = false;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (dead == false)
+        {
+            if (Input.GetButtonDown("Jump"))
+            {
+                rb.velocity = Vector2.zero;
+                rb.AddForce(new Vector2(0, 1) * (jumpForce));
+            }
+            rb.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+            cam.transform.Translate(new Vector3(1, 0, 0) * forwardSpeed * Time.deltaTime);
+        }
+        if(rb.transform.position.x >= 54)
+        {
+            dead = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        dead = true;
+    }
+
+    
+
+}
